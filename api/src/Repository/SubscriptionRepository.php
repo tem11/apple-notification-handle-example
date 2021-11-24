540821c175e83b08f71a095ffcb10ec54ee7a7bb
andriiboichuk <andboychuk12@gmail.com>
2021-11-24 16:42:23 +0200

    - cleanup autogen code

--- api/src/Repository/SubscriptionRepository.php
+++ api/src/Repository/SubscriptionRepository.php
@@ -25,33 +25,4 @@ class SubscriptionRepository extends ServiceEntityRepository
     {
         return $this->findOneBy(['subscriptionRef' => $subscriptionReference]);
     }
-
-    // /**
-    //  * @return Subscription[] Returns an array of Subscription objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Subscription
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
